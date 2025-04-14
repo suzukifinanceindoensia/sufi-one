e50f319af175e5ca2b52deb23187a63c0343b489
Grady Simanjaya <97289542+Gradydy@users.noreply.github.com>
2025-04-14 16:50:43 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'app/routes/app_routes.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 void main() {
+
   runApp(MyApp());
+  OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
+  OneSignal.initialize("INI KODE DARI ONESIGNAL");
+  OneSignal.Notifications.requestPermission(true);
 }
 
 class MyApp extends StatelessWidget {
