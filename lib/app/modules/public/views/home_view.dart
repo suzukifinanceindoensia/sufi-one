f5ae66a7d4b43b105906c5df7c337f07e888208f
Fadhil <Fadhillahsahad@gmail.com>
2025-04-15 17:32:08 +0700

    feat: color-constan, navbar

--- lib/app/modules/public/views/home_view.dart
+++ lib/app/modules/public/views/home_view.dart
@@ -1,27 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class PublicHomePage extends StatelessWidget {
+class NavBarColor {
+  static const Color background = Color.fromRGBO(245, 245, 245, 1);
+  static const Color icon = Color.fromRGBO(0, 38, 113, 1); // alpha 1.0 = 100%
+}
+
+class PublicHomePage extends StatefulWidget {
   const PublicHomePage({super.key});
+
+  @override
+  State<PublicHomePage> createState() => _PublicHomePageState();
+}
+
+class _PublicHomePageState extends State<PublicHomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const HomeContent(),
+    const Center(child: Text('Produk')),
+    const Center(child: Text('Chat')),
+    const Center(child: Text('Akun')),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Welcome to SuperApp')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Public Home - No Login Required'),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Get.toNamed('/public/catalog');
-              },
-              child: Text('Browse Catalog'),
-            ),
-          ],
-        ),
+      appBar: AppBar(title: const Text('Welcome to SuperApp')),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        backgroundColor: const Color.fromARGB(255, 90, 167, 255),
+        selectedItemColor: const Color.fromARGB(255, 0, 16, 49),
+        unselectedItemColor: const Color.fromARGB(
+          255,
+          255,
+          255,
+          255,
+        ).withOpacity(0.5),
+        type: BottomNavigationBarType.fixed,
+        onTap: (index) {
+          setState(() => _currentIndex = index);
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.inventory_2), label: ''),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat_bubble_outline),
+            label: '',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: ''),
+        ],
+      ),
+    );
+  }
+}
+
+class HomeContent extends StatelessWidget {
+  const HomeContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Public Home - No Login Required'),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Get.toNamed('/public/catalog');
+            },
+            child: const Text('Browse Catalog'),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/app/theme/color_constant.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class PrimaryColor {
+  static const Color snack = Color(0xFF4C7EFF);
+  static const Color splashStart = Color.fromRGBO(81, 120, 197, 1);
+  static const Color splashEnd = Color.fromRGBO(50, 132, 255, 1);
+}
+
+class NavBarColor {
+  static const Color background = Color.fromRGBO(245, 245, 245, 1);
+  static const Color icon = Color.fromRGBO(0, 38, 113, 80);
+}
+
+class IconColor {
+  static const Color defaultColor = Color.fromRGBO(44, 44, 44, 1);
+}
+
+class BackgroundColor {
+  static const Color bg1 = Color(0xFFFFFFFF);
+  static const Color bg2 = Color.fromRGBO(211, 230, 255, 1);
+  static const Color bg3 = Color.fromRGBO(43, 70, 125, 1);
+}
+
+class ButtonColor {
+  static const Color buttoncolor = Color.fromRGBO(43, 70, 125, 1);
+}
