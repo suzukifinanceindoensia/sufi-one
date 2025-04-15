e55b4157ee8d71d2d09ac0e09fcba7a27e1ebf82
Grady Simanjaya <97289542+Gradydy@users.noreply.github.com>
2025-04-15 16:20:31 +0700

    add:fontstyle dan implement ke main

---
+++ lib/app/theme/fontstyle.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyles {
+  static final TextStyle smallBody = GoogleFonts.outfit(
+    fontSize: 12,
+    color: Colors.black,
+  );
+  static final TextStyle medBody = GoogleFonts.outfit(
+    fontSize: 18,
+    color: Colors.black,
+  );
+  static final TextStyle bigBody = GoogleFonts.outfit(
+    fontSize: 24,
+    color: Colors.black,
+  );
+  static final TextStyle smallTitle = GoogleFonts.bebasNeue(
+    fontSize: 10,
+    color: Colors.red,
+  );
+  static final TextStyle medTitle = GoogleFonts.bebasNeue(
+    fontSize: 20,
+    color: Colors.red,
+  );
+  static final TextStyle bigTitle = GoogleFonts.bebasNeue(
+    fontSize: 30,
+    color: Colors.red,
+  );
+  static final TextStyle smallDisplay = GoogleFonts.emilysCandy(
+    fontSize: 16,
+    color: const Color.fromARGB(255, 54, 244, 70),
+  );
+  static final TextStyle medDisplay = GoogleFonts.emilysCandy(
+    fontSize: 16,
+    color: const Color.fromARGB(255, 54, 244, 70),
+  );
+  static final TextStyle bigDisplay = GoogleFonts.emilysCandy(
+    fontSize: 16,
+    color: const Color.fromARGB(255, 54, 244, 70),
+  );
+  static final TextStyle smallLabel = GoogleFonts.outfit(
+    fontSize: 10,
+    color: Colors.red,
+  );
+  static final TextStyle medLabel = GoogleFonts.outfit(
+    fontSize: 12,
+    color: Colors.red,
+  );
+  static final TextStyle bigLabel = GoogleFonts.outfit(
+    fontSize: 14,
+    color: Colors.red,
+  );
+  static final TextStyle smallHeadline = GoogleFonts.outfit(
+    fontSize: 14,
+    color: Colors.red,
+  );
+  static final TextStyle medHeadline = GoogleFonts.outfit(
+    fontSize: 14,
+    color: Colors.red,
+  );
+  static final TextStyle bigHeadline = GoogleFonts.outfit(
+    fontSize: 14,
+    color: Colors.red,
+  );
+}
