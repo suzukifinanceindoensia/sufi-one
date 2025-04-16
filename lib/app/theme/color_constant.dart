2a792659c3482084ce234dd416f3937cc7f66b49
Fadhil <Fadhillahsahad@gmail.com>
2025-04-16 10:36:23 +0700

    refactor: color constants menjadi AppColors class

--- lib/app/theme/color_constant.dart
+++ lib/app/theme/color_constant.dart
@@ -1,26 +1,23 @@
 import 'package:flutter/material.dart';
 
-class PrimaryColor {
+class AppColors {
+  // Primary Colors
   static const Color snack = Color(0xFF4C7EFF);
   static const Color splashStart = Color.fromRGBO(81, 120, 197, 1);
   static const Color splashEnd = Color.fromRGBO(50, 132, 255, 1);
-}
 
-class NavBarColor {
-  static const Color background = Color.fromRGBO(245, 245, 245, 1);
-  static const Color icon = Color.fromRGBO(0, 38, 113, 80);
-}
+  // NavBar Colors
+  static const Color navBackground = Color.fromRGBO(245, 245, 245, 1);
+  static const Color navIcon = Color.fromRGBO(0, 38, 113, 0.8);
 
-class IconColor {
-  static const Color defaultColor = Color.fromRGBO(44, 44, 44, 1);
-}
+  // Icon Colors
+  static const Color iconDefault = Color.fromRGBO(44, 44, 44, 1);
 
-class BackgroundColor {
+  // Background Colors
   static const Color bg1 = Color(0xFFFFFFFF);
   static const Color bg2 = Color.fromRGBO(211, 230, 255, 1);
   static const Color bg3 = Color.fromRGBO(43, 70, 125, 1);
-}
 
-class ButtonColor {
-  static const Color buttoncolor = Color.fromRGBO(43, 70, 125, 1);
+  // Button Colors
+  static const Color button = Color.fromRGBO(43, 70, 125, 1);
 }
