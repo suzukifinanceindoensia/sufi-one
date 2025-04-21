6f14ae5366c8ecf97747626658433ac1ceba86b0
Fadhil <Fadhillahsahad@gmail.com>
2025-04-21 09:15:56 +0700

    penghapusan mobile collect di branch homepage_public_view

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -2,7 +2,6 @@ import 'package:get/get.dart';
 import '../modules/public/views/splash_view.dart';
 import '../modules/public/views/home_view.dart';
 import '../modules/public/views/homepage_cust_view.dart';
-import '../modules/public/views/mobile_collection.dart'; // Ensure MobileCollection is correctly imported
 
 class AppRoutes {
   static const splash = '/';
@@ -14,6 +13,5 @@ class AppRoutes {
     GetPage(name: splash, page: () => SplashPage()),
     GetPage(name: publicHome, page: () => PublicHomePage()),
     GetPage(name: homepageCust, page: () => HomepageCust()),
-    GetPage(name: mobileCollection, page: () => MobileCollection()), // New page
   ];
 }
