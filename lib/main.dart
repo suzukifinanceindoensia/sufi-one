fb0859e18662bc69ebb25aa1cdb7f4e8aa299511
Fadhil <Fadhillahsahad@gmail.com>
2025-04-21 11:26:22 +0700

    develop mobcol

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'app/routes/app_routes.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'app/theme/fontstyle.dart';
+import 'app/modules/mobcol/views/mobile_collection_view.dart';
 
 void main() {
   runApp(MyApp());
@@ -38,8 +39,10 @@ class MyApp extends StatelessWidget {
         ),
       ),
       debugShowCheckedModeBanner: false,
-      initialRoute: AppRoutes.splash,
+      home: MobileCollection(),
+      initialRoute: AppRoutes.mobileCollection,
       getPages: AppRoutes.pages,
+      // initialRoute: AppRoutes.splash,
     );
   }
 }
