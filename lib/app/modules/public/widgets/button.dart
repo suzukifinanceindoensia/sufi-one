987733a8b1180b3e6052893afa081423182a104d
Grady Simanjaya <97289542+Gradydy@users.noreply.github.com>
2025-04-21 14:45:36 +0700

    add:memisahkan file widget

--- lib/app/modules/public/widgets/button.dart
+++ lib/app/modules/public/widgets/button.dart
@@ -34,12 +34,3 @@ class normalButton extends StatelessWidget {
   }
 }
 
-class Button {
-  static final ButtonStyle elevbutton = ElevatedButton.styleFrom(
-          backgroundColor: AppColors.button,
-          padding: EdgeInsets.zero, // Remove default padding
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(5)), // Make corners square
-          ),
-        );
-}
