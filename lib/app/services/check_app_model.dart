702de32931e7b753d867983cb8d9c1e75e371584
FADHILLAH SAHAD <fadhillah.sahad@sfi.itochu.co.id>
2025-05-22 13:44:04 +0700

    penambahan fitur peringatan berupa pop-up jika ada aplikasi fakeGPS yang terinstall dalam devica, dan print di terminal

--- lib/app/services/check_app_model.dart
+++ lib/app/services/check_app_model.dart
@@ -19,4 +19,22 @@ class AppModel {
       'versionCode': versionCode,
     };
   }
+
+  //daftar package name aplikasi fakeGPS
+  static const List<String> fakeGpsPackages = [
+    'com.lexa.fakegps',
+    'com.blogspot.newapphorizons.fakegps',
+    'com.just4funtools.fakegpslocationprofessional',
+    'com.hopefactory2021.fakegpslocation',
+    'com.mobile.fakelocation',
+    'fake.gps.location.emulator',
+    'top.tinysoft.fakegps',
+    'project.listick.fakegps',
+    'fakegps.fakelocation.mocklocation.gpsfaker',
+    'com.gsmartstudio.fakegps',
+    'com.evezzon.fakegps',
+    'com.discipleskies.mock_location_spoofer',
+    'com.mock.cartage',
+    'com.locationchanger',
+  ];
 }
