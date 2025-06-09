46d4ca3bf3ff40a90c503bc0dad56e3a2cd50638
FADHILLAH SAHAD <fadhillah.sahad@sfi.itochu.co.id>
2025-06-09 09:23:24 +0700

    penggunaan controller autentikasi di main untuk selalu refresh controller/menghindari error controller tidak running

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,13 @@ import 'package:get/get.dart';
 import 'package:sufi_one/app/modules/public/home_routes.dart';
 import 'app/routes/app_routes.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:sufi_one/app/auth/bindings/auth_binding.dart';
+import 'package:sufi_one/app/auth/controllers/auth_controller.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
+  Get.put(AuthController(), permanent: true);
   runApp(MyApp());
   OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
   OneSignal.initialize("INI KODE DARI ONESIGNAL");
@@ -20,6 +25,7 @@ class MyApp extends StatelessWidget {
       title: 'SuperApp (Public)',
       debugShowCheckedModeBanner: false,
       initialRoute: HomeRoutes.splash,
+      initialBinding: AuthBinding(),
       getPages: AppRoutes.pages,
     );
   }
