e00a4e38fa5a2211a651e06e6ccbf38738f869e4
GRADY SIMANJAYA <grady.simanjaya@sfi.itochu.co.id>
2025-06-13 09:41:51 +0700

    changing service to binding

--- lib/app/modules/mobcol/feature/uploadBukti/controller/mobcol_uploadbukti_view.dart
+++ lib/app/modules/mobcol/feature/uploadBukti/controller/mobcol_uploadbukti_view.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sufi_one/app/modules/mobcol/models/uploadBukti_model.dart';
@@ -5,6 +6,7 @@ import 'package:sufi_one/app/modules/mobcol/services/uploadBukti_service.dart';
 
 class MobcolUploadBuktiController extends GetxController {
   final UploadBuktiService _allUploadBuktiService = Get.find<UploadBuktiService>();
+
   final TextEditingController searchController = TextEditingController();
   final RxString searchText = ''.obs;
   final RxList<AllUploadBuktiModel> alluploadbukti = <AllUploadBuktiModel>[].obs;
@@ -13,18 +15,8 @@ class MobcolUploadBuktiController extends GetxController {
   final RxString errorMessage = ''.obs;
 
   final Map<int, String> monthsInYear = {
-    1: "January",
-    2: "February",
-    3: "March",
-    4: "April",
-    5: "May",
-    6: "June",
-    7: "July",
-    8: "August",
-    9: "September",
-    10: "October",
-    11: "November",
-    12: "December",
+    1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June",
+    7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December",
   };
 
   @override
@@ -41,9 +33,18 @@ class MobcolUploadBuktiController extends GetxController {
   }
 
   Future<void> _loadAllUploadBukti() async {
-    final data = await _allUploadBuktiService.getAllUploadBuktiFromJson();
-    alluploadbukti.value = data;
-    filtereduploadbukti.value = data;
+    try {
+      isLoading.value = true;
+      final data = await _allUploadBuktiService.getAllUploadBuktiFromJson();
+      alluploadbukti.value = data;
+      filtereduploadbukti.value = data;
+      errorMessage.value = ''; // Clear any previous error
+    } catch (e) {
+      errorMessage.value = 'Failed to load data: $e';
+      print('Error loading upload bukti: $e');
+    } finally {
+      isLoading.value = false;
+    }
   }
 
   void _onSearchChanged() {
